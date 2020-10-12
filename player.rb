4b3caa39e99e9c857a0d28d7659fdce84a35aff9
davemgj84 <davemgj@gmail.com>
2020-10-12 21:20:24 +0000

    player class complete

--- player.rb
+++ player.rb
@@ -3,4 +3,23 @@
 #   attr_reader :lives
 #   method that reduces lives
 #   Name ("Player 1")
-#   Lives ("3/3")
+#   lives ("3/3")
+
+class Player
+  attr_reader :name
+  attr_reader :lives
+
+  def initialize(name)
+    @name = name
+    @lives = 3
+  end
+
+  def death
+    @lives = @lives - 1
+  end
+
+  def format_lives
+    "#{lives}/3"
+  end
+
+end
